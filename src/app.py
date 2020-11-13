372e7715c1fb66f71b8fcb7c53871d7a96b724ba
felagorn <13519062@std.stei.itb.ac.id>
2020-11-13 20:15:29 +0700

    Upload File v1
    File bisa diupload!
    - Format yang diterima hanya .txt (dilakukan double-checking terhadap extension pada client dan server).
    - File disimpan di ./uploads/

--- src/app.py
+++ src/app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+app.config["MAX_CONTENT_LENGTH"] = 5120 * 5120
+app.config["UPLOAD_EXTENSIONS"] = [".txt"]
+app.config["UPLOAD_PATH"] = "uploads"
 
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/addfile", methods=["POST"])
+def addfile():
+    for txtfile in request.files.getlist("txtfile"):
+        filename = secure_filename(txtfile.filename)
+        if filename != "":
+            ext = os.path.splitext(filename)[1]
+            if ext not in app.config["UPLOAD_EXTENSIONS"]:
+                abort(400)
+            txtfile.save(os.path.join(app.config["UPLOAD_PATH"], filename))
+    return redirect(url_for('index'))
+
+@app.route("/addurl")
+def addurl():
+    return True
+
+@app.route("/search")
+def search():
+    return True
