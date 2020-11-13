6184acdc30d54387a7f10525dce6d511b5c77d97
felagorn <13519062@std.stei.itb.ac.id>
2020-11-13 22:55:19 +0700

    Web Scraping v1
    Web Scraping bisa untuk website kontrol (cnnindonesia.com)
    - Input dari user berupa URL "e.g. https://www.cnnindonesia.com/ekonomi/20201113080142-532-569263/blt-subsidi-gaji-untuk-271-juta-pekerja-cair-hari-ini"
    - Webpage akan disave dalam file .txt dengan format isi file:
    <Judul halaman>
    
    <Paragraf 1>
    <Paragraf 2>
    dst.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 from werkzeug.utils import secure_filename
 import os
+import requests
+from bs4 import BeautifulSoup
+import unicodedata
 
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 5120 * 5120
@@ -22,9 +25,22 @@ def addfile():
             txtfile.save(os.path.join(app.config["UPLOAD_PATH"], filename))
     return redirect(url_for('index'))
 
-@app.route("/addurl")
+@app.route("/addurl", methods=["POST"])
 def addurl():
-    return True
+    link = request.form.get("urlfile")
+    doc = requests.get(link)
+    soup = BeautifulSoup(doc.text, "html.parser")
+    title = soup.title.string
+    list_contentdetail = soup.find_all("div", "content_detail")
+    list_paragraph = list_contentdetail[0].find_all("p")
+    paragraph = []
+    for i in range(len(list_paragraph)):
+        paragraph.append(list_paragraph[i].text.strip() + "\n")
+        paragraph[i] = paragraph[i].replace(u"\xa0", u" ")
+    with open("./uploads/" + secure_filename(title) + ".txt", "w") as f:
+        f.write(title + "\n\n")
+        f.writelines(paragraph)
+    return redirect(url_for('index'))
 
 @app.route("/search")
 def search():
